ff335f88808be83b634de42ca00f3156f3615f05
darren siepka <D@siepka.me.uk>
2017-11-05 23:07:30 +0000

    added sport lever options , create modular compile options for display add further work for use with stm32 mcu

---
+++ gear_mini_v2_002/gear_mini_v2_002/utils.h
@@ -0,0 +1,16 @@
+/*
+These are some utility functions and variables used through the main code
+*/ 
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <Arduino.h>
+
+int freeRam ();
+void setPinMapping(byte boardID);
+
+//unsigned int PW();
+//unsigned int PW_SD();
+//unsigned int PW_AN();
+
+#endif // UTILS_H
